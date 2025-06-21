5eb0b3a2800e62c8bf3076907425ea65d9e729bb
voezd <volkan.live@hotmail.com>
2025-06-21 23:58:17 +0200

    adgs

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,6 +1,5 @@
 import { io } from "socket.io-client";
 
-const baseUrl = import.meta.env.PUBLIC_SOCKET_URL;
-export const socket = io(baseUrl, {
+export const socket = io("https://imposterselfmadebackend.onrender.com/", {
     autoConnect: false,
 });
